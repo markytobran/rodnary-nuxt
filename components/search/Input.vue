3d659504b4233f513222e641380e42bf0075dba5
Mark Bran <47259119+markytobran@users.noreply.github.com>
2023-12-28 18:22:07 +0000

    feat: add microphone functionality

--- components/search/Input.vue
+++ components/search/Input.vue
@@ -5,7 +5,7 @@
         v-model="searchText"
         type="text"
         placeholder="Carps, Baits, Tackles and More"
-        classes="border-t-2 border-l-2 border-b-2 h-14 py-3 px-4 w-full bg-transparent rounded-l-full outline-none text-white cursor-pointer"
+        classes="border-t-2 border-l-2 border-b-2 h-16 py-3 px-4 w-full bg-transparent rounded-l-full outline-none text-white cursor-pointer"
         @keydown.enter.prevent="searchSubmit"
       />
       <button v-if="showDeleteBtn" @click="resetInputField" class="absolute top-3 w-8 right-2 my-auto">
@@ -13,18 +13,29 @@
       </button>
     </div>
     <button
+      class="w-20 md:w-24 flex flex-col justify-center items-center border-t-2 border-b-2 border-l-2 cursor-pointer"
+      aria-label="Search by voice"
+      :class="isListening ? 'pt-2 bg-average-pink' : 'bg-secondary-color'"
+      @click="listenUser"
+    >
+      <IconMicrophone :class="isListening ? 'animate-bounce' : ''" />
+      <span v-if="isListening" class="text-sm font-bold">On</span>
+    </button>
+    <button
       class="w-20 md:w-24 flex justify-center items-center border-2 rounded-r-full"
       type="submit"
+      aria-label="Search"
       :disabled="props.isLoading"
       :class="isLoading ? 'bg-slate-400 cursor-not-allowed' : 'bg-secondary-color cursor-pointer'"
     >
-      <IconMagnifier class="text-white" />
+      <IconMagnifier />
     </button>
   </form>
 </template>
 
 <script setup lang="ts">
 const searchText = ref('')
+const isListening = ref(false)
 const showDeleteBtn = computed(() => searchText.value.length > 0)
 const resetInputField = () => {
   searchText.value = ''
@@ -42,5 +53,20 @@ const props = defineProps({
 const searchSubmit = () => {
   const encoded = encodeURI(searchText.value)
   router.push('/search?q=' + encoded)
+}
+
+const listenUser = () => {
+  isListening.value = true
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+  const rec = new SpeechRecognition()
+  rec.lang = 'en-US'
+
+  rec.onresult = (e: any) => {
+    searchText.value = e.results[0][0].transcript
+    searchSubmit()
+    isListening.value = false
+  }
+
+  rec.start()
 }
 </script>
