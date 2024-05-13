94c66cfa1713adba601f6981d4dd2d21f7072f00
Mark Bran <47259119+markytobran@users.noreply.github.com>
2024-05-13 13:45:09 +0100

    fix: div tag

--- components/UI/base/VideoSlider.server.vue
+++ components/UI/base/VideoSlider.server.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4 pt-10 carousel-height">
-    <div v-for="n in 4" :key="n" class="content-loading h-44"></div>
+    <div v-for="n in 4" :key="n" class="content-loading h-44"/>
   </div>
 </template>
